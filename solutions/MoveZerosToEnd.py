be6f70b8808a10bf358935b327f62f74b60ed1d5
ninochang <nino.chang@swag.live>
2021-02-20 15:25:58 +0800

    Modify array inplace

--- solutions/MoveZerosToEnd.py
+++ solutions/MoveZerosToEnd.py
@@ -8,15 +8,29 @@ eq. input: [1,2,0,6,5,0,10]
 '''
 
 def move_end(array):
-    count = 0
-    for n in array:
-        if n == 0:
-            count += 1
+    def swap(i, j):
+        array[i], array[j] = array[j], array[i]
 
-    array[:] =[i for i in array if i!=0 ]
-    array += [0]*count
+    for i in range(len(array)):
+        if array[i] == 0:
+            zero_index = i
+            break
+        else:
+            continue
+
+    if i == len(array) - 1:
+        return array
+
+    current_index = zero_index
+    while current_index < len(array):
+        if array[current_index] != 0:
+            swap(zero_index, current_index)
+            zero_index += 1
+        current_index += 1
+    return array
+
 
 if __name__ == '__main__':
     array = [1,2,0,6,5,0,10]
     move_end(array)
-    print array
+    print(array)
